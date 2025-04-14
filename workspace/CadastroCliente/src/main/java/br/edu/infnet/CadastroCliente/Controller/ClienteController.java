ef553e5c5e932af564fcd325b117aa7988116d6e
JonesAugusto1989 <jassp1989@gmail.com>
2025-04-13 23:50:12 -0300

    Alterando o workflow

--- workspace/CadastroCliente/src/main/java/br/edu/infnet/CadastroCliente/Controller/ClienteController.java
+++ workspace/CadastroCliente/src/main/java/br/edu/infnet/CadastroCliente/Controller/ClienteController.java
@@ -4,7 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,17 +26,25 @@ public class ClienteController {
 	
 	
 	@GetMapping("/")
-	public List<Cliente> findAll(HttpServletRequest request) {
+	public ResponseEntity<List<Cliente>>  findAll(HttpServletRequest request) {
 		System.out.println("entrou no controler");
 		criaSessao(request);
-		List<Cliente> clientListFound = clienteService.findAllCliente();		
-		return clientListFound;
+		List<Cliente> clientListFound = clienteService.findAllCliente();	
+		
+		return ResponseEntity.ok(clientListFound);
 	}
 	
 	@GetMapping("/cria-sessao")
 	public String criaSessao(HttpServletRequest request) {
 	    request.getSession(true).setAttribute("Sessão", "pt-BR");
 	    return "Sessão criada!";
+	}
+	
+	@PostMapping("/incluircliente")
+	public ResponseEntity<String> incluirCliente(HttpServletRequest request,@RequestBody Cliente cliente) {
+		criaSessao(request);
+		clienteService.saveCliente(cliente);
+		return ResponseEntity.status(HttpStatus.CREATED).body("Cliente criado com sucesso");
 	}
 
 }
