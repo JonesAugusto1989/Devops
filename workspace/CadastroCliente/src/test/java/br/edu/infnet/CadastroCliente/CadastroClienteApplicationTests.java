7b667a6591bc2cabc348bc27cb8380ab7d6d3728
JonesAugusto1989 <jassp1989@gmail.com>
2025-04-15 01:28:45 -0300

    Alterando workflow

--- workspace/CadastroCliente/src/test/java/br/edu/infnet/CadastroCliente/CadastroClienteApplicationTests.java
+++ workspace/CadastroCliente/src/test/java/br/edu/infnet/CadastroCliente/CadastroClienteApplicationTests.java
@@ -1,8 +1,14 @@
 package br.edu.infnet.CadastroCliente;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import br.edu.infnet.CadastroCliente.Controller.ClienteController;
+import br.edu.infnet.CadastroCliente.Model.Cliente;
+import jakarta.servlet.http.HttpServletRequest;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -11,5 +17,39 @@ class CadastroClienteApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	
+	@Autowired
+    private ClienteController clienteController;
+	private HttpServletRequest request;
+   
+
+
+    private Cliente cliente1;
+    private Cliente cliente2;
+
+    @BeforeEach
+    void setUp() {
+  
+   
+        cliente1 = new Cliente();
+        cliente1.setNome("João da Silva");
+        cliente1.setCpfCnpj("12345678901");
+        cliente1.setEmail("joao@email.com");
+        cliente1.setTelefone("123456789");
+
+        cliente2 = new Cliente();
+        cliente2.setNome("Maria Souza");
+        cliente2.setCpfCnpj("98765432109");
+        cliente2.setEmail("maria@email.com");
+        cliente2.setTelefone("987654321");
+        
+       
+    }
+    
+    @Test
+    void incluirCliente() {
+    	clienteController.incluirCliente(request, cliente1);
+    	clienteController.incluirCliente(request, cliente2);
+    }
 
 }
